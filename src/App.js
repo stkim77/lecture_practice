9712b7db1e4f43b976767fef280ad8bf9d14c4d9
Logan[sungtae kim] <stkim77@coupang.com>
2020-05-10 14:09:36 +0900

    draw draft ui

--- src/App.js
+++ src/App.js
@@ -10,17 +10,21 @@ function App() {
       </header>
       <div className='App-body'>
         <div className='comment_container'>
-          <div className='contents'>
-            <div>
-              <img src={icon} alt='icon' />
-            </div>
-            <div className='name'>이름</div>
-            <div className='comment'>코멘트</div>
+          <div className='icon'>
+            <img src={icon} alt='icon' />
           </div>
-          <div className='additional_info'>
-            <div className='like'>좋아요</div>
-            <div className='num_like'>갯수</div>
-            <div className='reg_time'>몇분전</div>
+          <div className='contents'>
+            <div className='main_info'>
+              <div className='name'>로건</div>
+              <div className='comment'>테스트 코멘트</div>
+            </div>
+            <div className='additional_info'>
+              <div className='like'>좋아요</div>
+              <span>&nbsp;·&nbsp;</span>
+              <div className='num_like'>2개</div>
+              <span>&nbsp;·&nbsp;</span>
+              <div className='reg_time'>5분전</div>
+            </div>
           </div>
         </div>
       </div>
